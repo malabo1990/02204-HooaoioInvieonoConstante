3d36fca563e256eed254d029b5ad2d63399d3c8f
malabo1990 <malabo1990@gmail.com>
2019-11-13 23:04:21 +0100

    Add files via upload

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+
+/**
+ *
+ * @author Casimiro Ondo Obiang casimiro.ondo.alum@iescamp.es
+ */
+public class Main {
+    public static void main(String[] args) {
+    
+        //Ejercicio: 02204-HorarioInviernoConstante
+        final boolean HORARIO_INVIERNO = true;
+        System.out.println("Horario de  invierno....:"+HORARIO_INVIERNO);
+    }
+}
